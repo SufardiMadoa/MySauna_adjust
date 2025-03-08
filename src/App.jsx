ca2db75816b068db091e496a5d4cc79cd6e9305f
SufardiMadoa <Sufardimpv@gmail.com>
2025-03-08 13:44:57 +0800

    feat: uppdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar.jsx';
+import Footer from './components/layout/Footer.jsx';
 import Home from './pages/Home';
 
 function App() {
